e032181ee03862ddbfeb169126e401fb93be2dc9
abhinavm07 <88830300+abhinavm07@users.noreply.github.com>
2022-12-17 00:22:36 +0545

    Testing Data

--- controllers/logic.js
+++ controllers/logic.js
@@ -4,16 +4,35 @@ const homePage = (req, res) => {
   // throw new Error("Testing Express async error package");
 
   res.send("Welcome to the Home Page");
+
   console.log("Hi Man");
 };
 
 const getUser = async (req, res) => {
-  const dataOut = await data.create({
-    name: "Abhinab",
-    username: "_abhinavm7",
-  });
-
-  res.status(200).json({ msg: dataOut });
+  try {
+    const dataOut = await data.create(req.query);
+    res.status(200).json({ msg: dataOut });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-module.exports = { homePage, getUser };
+const getStatic = async (req, res) => {
+  try {
+    const dataOut = await data.find({});
+    res.status(200).json({ msg: dataOut });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addData = async (req, res) => {
+  try {
+    const dataOut = await data.create(req.query);
+    res.status(200).json({ msg: "Successful !" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { homePage, getStatic, getUser, addData };
--- routes/routeTasks.js
+++ routes/routeTasks.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const Router = express.Router();
-const { homePage, getUser } = require("../controllers/logic");
+const {
+  homePage,
+  getStatic,
+  getUser,
+  addData,
+} = require("../controllers/logic");
 
 Router.get("/", homePage);
+Router.get("/allData", getStatic);
+Router.get("/add", addData);
 Router.get("/username", getUser);
 
 module.exports = Router;
