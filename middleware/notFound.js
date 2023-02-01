efdbdd9f57723b05e6535a4b1ecaf374f0171ffa
abhinavm07 <88830300+abhinavm07@users.noreply.github.com>
2023-02-01 23:24:06 +0545

    update

--- middleware/notFound.js
+++ middleware/notFound.js
@@ -1,7 +1,5 @@
 const notfound = (req, res) => {
-  res.send(
-    `Sorry, The Route you are searching for doesn't exist ! Please Try Something else`
-  );
+  res.send(`Sorry, The Route you are searching for doesn't exist !`);
 };
 
 module.exports = notfound;
