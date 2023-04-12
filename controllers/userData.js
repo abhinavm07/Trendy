580f8722cdb773672c119cc1c74215bc1bbc4cfc
abhinavm07 <88830300+abhinavm07@users.noreply.github.com>
2023-04-12 20:54:01 +0545

    user data backend

--- controllers/userData.js
+++ controllers/userData.js
@@ -1,10 +1,10 @@
 const { TwitterApi } = require("twitter-api-v2");
 require("dotenv/config");
 
-const apiKey = process.env.API_KEY;
-const apiSecret = process.env.API_KEY_SECRET;
-const apiTokenkey = process.env.API_TOKEN;
-const apiTokenSecret = process.env.API_TOKEN_SECRET;
+// const apiKey = process.env.API_KEY;
+// const apiSecret = process.env.API_KEY_SECRET;
+// const apiTokenkey = process.env.API_TOKEN;
+// const apiTokenSecret = process.env.API_TOKEN_SECRET;
 
 const aposToLexForm = require("apos-to-lex-form");
 const { WordTokenizer, SentimentAnalyzer, PorterStemmer } = require("natural");
@@ -18,34 +18,52 @@ spellCorrector.loadDictionary();
 const analyzer = new SentimentAnalyzer("English", PorterStemmer, "afinn");
 
 const client = new TwitterApi(`${process.env.BEARER_KEY}`);
+let contextByID = [];
 
 const getTwtData = async (req, res) => {
   const { twtUsername } = req.body;
   let tweetsColec = [];
-  let twtObj = {};
-  let twtRes = {};
+  let userAnnotes = [];
+
   const user = await client.v2.userByUsername(twtUsername, {
     "user.fields": "public_metrics",
   });
   const userId = user?.data?.id || null;
   if (userId) {
-    //const userFollowers = await client.v2.followers(`${userId}`);
-    // const userFollowing = await client.v2.following(`${userId}`);
     const userTweets = await client.v2.userTimeline(`${userId}`, {
       exclude: "retweets,replies",
+      "tweet.fields": "context_annotations",
     });
     if (userTweets) {
       let twtData = userTweets["_realData"]["data"];
+      // console.log(twtData);
       for (const twt of twtData) {
+        const id = twt["id"];
         const twtSentiment = await tweetsSentiment(twt["text"]);
-        tweetsColec.push({
-          id: twt["id:"],
-          tweet: twt["text"],
-          sentiment: twtSentiment,
-        });
+        const annotations = twt["context_annotations"];
+        if (annotations) {
+          const context = tweetContext(annotations, id);
+          userAnnotes.push(context[id]);
+          tweetsColec.push({
+            id: twt["id"],
+            tweet: twt["text"],
+            sentiment: twtSentiment,
+            context: context[id],
+          });
+        } else {
+          tweetsColec.push({
+            id: twt["id"],
+            tweet: twt["text"],
+            sentiment: twtSentiment,
+          });
+        }
       }
-      console.log(tweetsColec);
-      return res.status(200).json({ userData: user, twtData: tweetsColec });
+      const contextVolume = await contextVol(userAnnotes.flat());
+      return res.status(200).json({
+        userData: user,
+        twtData: tweetsColec,
+        contextVolume: contextVolume,
+      });
     }
     return res
       .status(404)
@@ -65,18 +83,14 @@ const searchTwt = async (req, res) => {
   const searchTweets = await client.v2.search(search, {
     "media.fields": "url",
   });
-
-  // Consume every possible tweet of jsTweets (until rate limit is hit)
   for await (const tweet of searchTweets) {
     i++;
     if (i < 20) {
       tweetColec.push([tweet.id, tweet.text]);
     } else if (i >= 20) {
-      console.log(i);
       break;
     }
   }
-  console.log(i);
   tweetColec.push([`recent tweet count : ${recentTweets.data[0].tweet_count}`]);
   res.json(tweetColec);
 };
@@ -87,20 +101,14 @@ const tweetsSentiment = async (data) => {
   let sentimentRemark;
 
   if (sentiment === 1) {
-    sentimentRemark = "Positive";
-    // console.log("Positive");
     return "Positive";
   }
 
   if (sentiment === 0) {
-    sentimentRemark = "Neutral";
-    // console.log("Neutral");
     return "Neutral";
   }
 
   if (sentiment === -1) {
-    sentimentRemark = "Negative";
-    // console.log("Negative");
     return "Negative";
   }
 };
@@ -121,12 +129,37 @@ const getSentiment = (data) => {
   const stopWordsRemoved = stopword.removeStopwords(fixedSpelling);
 
   const analyzed = analyzer.getSentiment(stopWordsRemoved);
-  console.log(stopWordsRemoved);
-  console.log(analyzed);
+  // console.log(stopWordsRemoved);
+  // console.log(analyzed);
   if (analyzed > 0) return 1; // positive
   if (analyzed === 0) return 0;
   if (isNaN(analyzed)) return 0;
   return -1;
+};
+
+//
+
+const tweetContext = (tweets, id) => {
+  tweets.forEach((element) => {
+    if (contextByID[id]) {
+      if (!contextByID[id].includes(element["entity"]["name"])) {
+        contextByID[id].push(element["entity"]["name"]);
+      }
+    } else {
+      contextByID[id] = [element["entity"]["name"]];
+    }
+  });
+  return contextByID;
+};
+let contextVols = {};
+const contextVol = (annots) => {
+  contextVols = {};
+  console.log(contextVols);
+  annots.forEach((element) => {
+    contextVols[element] = (contextVols[element] || 0) + 1;
+  });
+  console.log(contextVols);
+  return contextVols;
 };
 
 module.exports = { getTwtData, searchTwt };
